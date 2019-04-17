1ce7649a49317629e745f59990d63ac8f3ed21a2
wangxuefeng <wangxuefeng2@xiaoniu66.com>
2019-04-17 10:34:44 +0800

    添加framework

---
+++ SweetAlert/SweetAlert.h
@@ -0,0 +1,19 @@
+//
+//  SweetAlert.h
+//  SweetAlert
+//
+//  Created by xiaoniu on 2019/4/17.
+//  Copyright © 2019 com.learn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SweetAlert.
+FOUNDATION_EXPORT double SweetAlertVersionNumber;
+
+//! Project version string for SweetAlert.
+FOUNDATION_EXPORT const unsigned char SweetAlertVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SweetAlert/PublicHeader.h>
+
+
